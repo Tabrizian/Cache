8d081bd8552e41a0470fc861cb5f4a7c6a7d5239
Iman Tabrizian <tabrizian@outlook.com>
2016-03-21 15:38:45 +0430

    Adds data_array.vhd implementation.

--- src/data_array.vhd
+++ src/data_array.vhd
@@ -1,24 +1,32 @@
 LIBRARY IEEE;
 use IEEE.std_logic_1164.all;
+use IEEE.std_logic_unsigned.all;
 
 entity data_array is
     port(clk, wren:in STD_LOGIC;
          address:in STD_LOGIC_VECTOR(5 downto 0);
-         wrdata:in STD_LOGIC_VECTOR(63 downto 0);
-         data:in STD_LOGIC_VECTOR(63 downto 0));
+         wrdata:in STD_LOGIC_VECTOR(31 downto 0);
+         data:out STD_LOGIC_VECTOR(31 downto 0));
 end data_array;
 
-architecture gate_level_data_array of data_array is
- --   component data_array is
- --       port(clk, wren:in STD_LOGIC;
- --            address:in STD_LOGIC_VECTOR(5 downto 0);
- --            wrdata:in STD_LOGIC_VECTOR(63 downto 0);
- --            data:in STD_LOGIC_VECTOR(63 downto 0));
- --   end component;
-
+architecture behavorial_data_array of data_array is
+    --   component data_array is
+    --       port(clk, wren:in STD_LOGIC;
+    --            address:in STD_LOGIC_VECTOR(5 downto 0);
+    --            wrdata:in STD_LOGIC_VECTOR(63 downto 0);
+    --            data:in STD_LOGIC_VECTOR(63 downto 0));
+    --   end component;
+    type data_array_data is array (63 downto 0) of STD_LOGIC_VECTOR (31 downto 0);
+    signal data_array : data_array_data;
 begin
     process(clk)
     begin
+        if(wren = '1') then
+            data <= wrdata;
+            data_array(conv_integer(address)) <= wrdata;
+        else
+            data <= data_array(conv_integer(address));
+        end if;
     end process;
 
-end gate_level_data_array;
+end behavorial_data_array;
