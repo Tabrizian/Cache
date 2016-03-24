e5ec085dc0672cbf6afae2f64a13f178604a765c
Iman Tabrizian <tabrizian@outlook.com>
2016-03-24 20:17:30 +0430

    Adds mux.

---
+++ src/mux.vhd
@@ -0,0 +1,19 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+entity mux is
+    port(sel:in STD_LOGIC;
+         w0:in STD_LOGIC_VECTOR(31 downto 0);
+         w1:in STD_LOGIC_VECTOR(31 downto 0);
+         output: out STD_LOGIC_VECTOR(31 downto 0)
+     );
+end entity;
+
+architecture behavioral of mux is
+begin
+    with sel select
+        output <= w0 when '0',
+                  w1 when '1',
+                  "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX" when others;
+end behavioral;
