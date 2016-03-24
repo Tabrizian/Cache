0d4e44d3cd8c9a5451ee4b7129bed56e122a1155
Iman Tabrizian <tabrizian@outlook.com>
2016-03-24 20:24:53 +0430

    Testbench prototype finished.

--- test/cache_tb.vhd
+++ test/cache_tb.vhd
@@ -7,6 +7,19 @@ entity cache_tb is
 
 architecture test_bench of cache_tb is
     component cache is
+        port(clk, wren, reset_n :in STD_LOGIC;
+             full_address :in STD_LOGIC_VECTOR(9 downto 0);
+             wrdata :in STD_LOGIC_VECTOR(31 downto 0);
+             validity :in STD_LOGIC;
+             data: out STD_LOGIC_VECTOR(31 downto 0);
+             hit: out STD_LOGIC
+         );
     end component;
+    signal clk, wren, reset_n : STD_LOGIC;
+    signal full_address : STD_LOGIC_VECTOR(9 downto 0);
+    signal wrdata : STD_LOGIC_VECTOR(31 downto 0);
+    signal validity : STD_LOGIC;
+    signal data: STD_LOGIC_VECTOR(31 downto 0);
+    signal hit: STD_LOGIC;
 begin
 end test_bench;
