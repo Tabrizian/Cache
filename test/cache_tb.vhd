933420ccf019c453d4abf978ccaba4173d770645
Iman Tabrizian <tabrizian@outlook.com>
2016-03-24 21:42:06 +0430

    Adds testbench for cache.

--- test/cache_tb.vhd
+++ test/cache_tb.vhd
@@ -10,16 +10,32 @@ architecture test_bench of cache_tb is
         port(clk, wren, reset_n :in STD_LOGIC;
              full_address :in STD_LOGIC_VECTOR(9 downto 0);
              wrdata :in STD_LOGIC_VECTOR(31 downto 0);
-             validity :in STD_LOGIC;
+             validate, invalidate:in STD_LOGIC;
              data: out STD_LOGIC_VECTOR(31 downto 0);
              hit: out STD_LOGIC
-         );
+    );
     end component;
-    signal clk, wren, reset_n : STD_LOGIC;
+    signal clk : STD_LOGIC := '0';
+    signal wren, reset_n : STD_LOGIC;
     signal full_address : STD_LOGIC_VECTOR(9 downto 0);
     signal wrdata : STD_LOGIC_VECTOR(31 downto 0);
-    signal validity : STD_LOGIC;
+    signal validate, invalidate: STD_LOGIC;
     signal data: STD_LOGIC_VECTOR(31 downto 0);
     signal hit: STD_LOGIC;
 begin
+    mapping : cache port map(clk,wren,reset_n,full_address,wrdata,validate,invalidate,data,hit);
+
+    full_address <= STD_LOGIC_VECTOR(to_unsigned(0,10)) after 0 ns,
+                    STD_LOGIC_VECTOR(to_unsigned(45,10)) after 1 ns,
+                    STD_LOGIC_VECTOR(to_unsigned(0,10)) after 2 ns;
+    wren <= '1' after 0 ns, '0' after 2 ns;
+    wrdata <= STD_LOGIC_VECTOR(to_unsigned(9, 32)) after 0 ns,
+              STD_LOGIC_VECTOR(to_unsigned(24,32)) after 1 ns;
+    reset_n <= '0' after 0 ns;
+    validate <= '1' after 0 ns;
+    invalidate <= '0' after 0 ns;
+
+    CLOCK:
+    clk <= '1' after 1 ns when clk = '0' else
+           '0' after 1 ns when clk = '1';
 end test_bench;
