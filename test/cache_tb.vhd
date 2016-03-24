944b0207dcf540eeb12bf004d28b90d442f3d4d4
Iman Tabrizian <tabrizian@outlook.com>
2016-03-24 20:22:40 +0430

    Adds test bench of cache architecture.

---
+++ test/cache_tb.vhd
@@ -0,0 +1,12 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+entity cache_tb is
+    end cache_tb;
+
+architecture test_bench of cache_tb is
+    component cache is
+    end component;
+begin
+end test_bench;
