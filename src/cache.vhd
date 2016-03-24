b71dad4aee58f2513c687b36b3a368f66219411e
Iman Tabrizian <tabrizian@outlook.com>
2016-03-24 18:58:07 +0430

    Simple module instantiation.

--- src/cache.vhd
+++ src/cache.vhd
@@ -3,14 +3,22 @@ use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
 entity cache is
-    port(clk, wren :in STD_LOGIC;
+    port(clk, wren, reset_n :in STD_LOGIC;
          full_address :in STD_LOGIC_VECTOR(9 downto 0);
          wrdata :in STD_LOGIC_VECTOR(31 downto 0);
          validity :in STD_LOGIC;
-         data: out STD_LOGIC_VECTOR(31 downto 0)
+         data: out STD_LOGIC_VECTOR(31 downto 0);
+         hit: in STD_LOGIC
      );
 end cache;
 
-architecture dataflow of cache is
+architecture gate_level of cache is
+    type data_array_data is array (63 downto 0) of STD_LOGIC_VECTOR (31 downto 0);
+    signal k0_data : data_array_data;
+    signal k1_data : data_array_data;
+    signal k0_wren : STD_LOGIC;
+    signal k1_wren : STD_LOGIC;
 begin
-end dataflow;
+    k0_data_array: data_array port map(clk => clk,k0_wren => wren,
+                                       wrdata => wrdata, k0_data => data);
+end gate_level;
