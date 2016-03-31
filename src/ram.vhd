a55e4dcb8f19e98c78d33e75d2a5dc897d432080
Iman Tabrizian <tabrizian@outlook.com>
2016-03-31 18:06:17 +0430

    Improves test_bench.

--- src/ram.vhd
+++ src/ram.vhd
@@ -5,24 +5,28 @@ use IEEE.numeric_std.all;
 entity ram is
     port(clk, wr:in STD_LOGIC;
          -- Address size is equal to index * 2 + tag
-         address:in STD_LOGIC_VECTOR(15 downto 0);
+         address:in STD_LOGIC_VECTOR(9 downto 0);
          data_in:in STD_LOGIC_VECTOR(31 downto 0);
          data_out:out STD_LOGIC_VECTOR(31 downto 0);
+         ram_ready:out STD_LOGIC := '0';
          data_read:out STD_LOGIC
      );
 end ram;
 
 architecture behavorial_data_array of ram is
-    type data_array_data is array (65535 downto 0) of STD_LOGIC_VECTOR (31 downto 0);
+    type data_array_data is array (1023 downto 0) of STD_LOGIC_VECTOR (31 downto 0);
     signal data_array : data_array_data;
 begin
+    data_out <= data_array(to_integer(unsigned(address)));
     process(clk)
     begin
+        ram_ready <= '0';
         if(wr = '1') then
             data_array(to_integer(unsigned(address))) <= data_in;
         end if;
 
-        data_out <= data_array(to_integer(unsigned(address)));
+        ram_ready <= '1';
+
     end process;
 
 end behavorial_data_array;
